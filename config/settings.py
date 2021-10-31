f7aac71ce77021aa6a78ab705abde58a587aabdb
Samandar Sariboyev <79693410+samandaruzbekistan@users.noreply.github.com>
2021-10-31 22:17:35 +0500

    Heroku uchun o'zgartirishlar

--- config/settings.py
+++ config/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-mw%2pqo$s27087mzhg#d6*9#==4m=9v9_i(ulz&0r)@j-pq$lb
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
