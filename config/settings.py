70d11983c69291ea2e36c90443cbec2645d01eb5
Samandar Sariboyev <79693410+samandaruzbekistan@users.noreply.github.com>
2021-10-31 23:28:42 +0500

    Deploy

--- config/settings.py
+++ config/settings.py
@@ -125,8 +125,8 @@ STATICFILES_DIRS = [
     BASE_DIR / "static",
 ]
 
-django_heroku.settings(locals())
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
-
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
