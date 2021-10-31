b9b15e8f5792413b176c52f100d4f765086a0633
Samandar Sariboyev <79693410+samandaruzbekistan@users.noreply.github.com>
2021-10-31 23:25:47 +0500

    deploy

--- config/settings.py
+++ config/settings.py
@@ -11,8 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
-import django_heroku
-import dj_database_url
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -130,4 +129,4 @@ django_heroku.settings(locals())
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
